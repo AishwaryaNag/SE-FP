b1ac0230ae1df788f46cb675a0a7cabb4373b158
AishwaryaNag <nagaraj.ai@northeastern.edu>
2023-11-29 00:26:11 -0500

    .

---
+++ src/main/java/Actor.java
@@ -0,0 +1,43 @@
+
+public class Actor {
+	private String actorID;
+	private String role;
+	private String competence;
+	private String location;
+
+	/**
+	 * 
+	 * @param actionDetails
+	 */
+	public void performAction(String actionDetails) {
+		// TODO - implement Actor.performAction
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param newLocation
+	 */
+	public void updateLocation(String newLocation) {
+		// TODO - implement Actor.updateLocation
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param statusDetails
+	 */
+	public void reportStatus(String statusDetails) {
+		// TODO - implement Actor.reportStatus
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param supportType
+	 */
+	public void requestSupport(String supportType) {
+		// TODO - implement Actor.requestSupport
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/main/java/Agency.java
@@ -0,0 +1,36 @@
+
+public class Agency {
+	private String agencyID;
+	private String name;
+	private String location;
+	private String contactInfo;
+	private float operationalBudget;
+
+	/**
+	 * 
+	 * @param emergencyID
+	 */
+	public void coordinateResponse(String emergencyID) {
+		// TODO - implement Agency.coordinateResponse
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param newInfo
+	 */
+	public void updateAgencyInfo(String newInfo) {
+		// TODO - implement Agency.updateAgencyInfo
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param reportType
+	 */
+	public void generateReport(String reportType) {
+		// TODO - implement Agency.generateReport
+		throw new UnsupportedOperationException();
+	}
+
+}
